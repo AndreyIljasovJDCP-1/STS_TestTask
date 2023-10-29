68c48dc6e9f6f57ec7caa7d1ceef6cbb06a1b500
Andrey Iljasov <a_iljasov@mail.ru>
2023-10-29 20:04:20 +0300

    Logger add, checkMaxLines optimize

--- src/test/java/org/example/AuditManagerTest.java
+++ src/test/java/org/example/AuditManagerTest.java
@@ -3,6 +3,8 @@ package org.example;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -16,9 +18,9 @@ import java.util.stream.Stream;
 class AuditManagerTest {
     private static final String DEFAULT_FILE_NAME = "audit_0.csv";
     private static final Path DIRECTORY_PATH = Path.of("src", "main", "resources");
-    private static final int MAX_LINES = 2;
+    private static final int MAX_LINES = 3;
     private AuditManager auditManager;
-
+    private static final Logger logger = LoggerFactory.getLogger(AuditManagerTest.class);
 
     @BeforeEach
     void SetUp() {
@@ -30,7 +32,6 @@ class AuditManagerTest {
     @ParameterizedTest(name = "Records add: {0}")
     @ValueSource(ints = {0, 1, 2, 3, 4, 5, 11})
     void check(int records) throws IOException {
-
         for (int i = 0; i < records; i++) {
             auditManager.addRecord("user" + (i + 1), LocalDateTime.now());
         }
@@ -39,8 +40,8 @@ class AuditManagerTest {
             filePaths = sorted.toList();
         }
         int expectedFiles = records / MAX_LINES + (records % MAX_LINES == 0 ? 0 : 1);
+        logger.info("Records: {} Count files: {}", records, expectedFiles);
         Assertions.assertEquals(expectedFiles, filePaths.size());
-
     }
 
     @Order(2)
@@ -48,27 +49,27 @@ class AuditManagerTest {
     @ParameterizedTest(name = "Records add: {0}")
     @ValueSource(ints = {0, 1, 2, 3, 4, 5, 11})
     void checkMaxLinesPerFile(int records) throws IOException {
-
         for (int i = 0; i < records; i++) {
             auditManager.addRecord("user" + (i + 1), LocalDateTime.now());
         }
-
         List<Path> filePaths;
         try (Stream<Path> sorted = Files.list(DIRECTORY_PATH).sorted()) {
             filePaths = sorted.toList();
         }
         int expectedLines;
-        int res = records;
-        for (Path path : filePaths) {
+        for (int i = 0; i < filePaths.size(); i++) {
+            Path path = filePaths.get(i);
             var listRecords = Files.readAllLines(path);
-            if (res >= MAX_LINES) {
-                expectedLines = MAX_LINES;
-                res -= MAX_LINES;
-            } else {
-                expectedLines = records % MAX_LINES;
+            expectedLines = MAX_LINES;
+            if (i == filePaths.size() - 1) {
+                expectedLines = records % MAX_LINES == 0
+                        ? MAX_LINES
+                        : records % MAX_LINES;
             }
-            int finalExpectedLines = expectedLines;
-            Assertions.assertEquals(finalExpectedLines, listRecords.size());
+            logger.info("FileName: {}  ", path.getFileName());
+            logger.info("File content: {}  ", listRecords);
+            logger.info("File size expected: {} File size actual: {}  ", expectedLines, listRecords.size());
+            Assertions.assertEquals(expectedLines, listRecords.size());
         }
     }
 
@@ -77,18 +78,18 @@ class AuditManagerTest {
     @ParameterizedTest(name = "Records add: {0}")
     @ValueSource(ints = {0, 1, 2, 3, 4, 5, 11})
     void checkFileName(int records) throws IOException {
-
         for (int i = 0; i < records; i++) {
             auditManager.addRecord("user" + (i + 1), LocalDateTime.now());
         }
-
         List<Path> filePaths;
         try (Stream<Path> sorted = Files.list(DIRECTORY_PATH).sorted()) {
             filePaths = sorted.toList();
         }
         for (int i = 0; i < filePaths.size(); i++) {
+            Path path = filePaths.get(i);
             String expectedFileName = DEFAULT_FILE_NAME.replaceFirst("0", i + "");
-            String actualFileName = filePaths.get(i).getFileName().toString();
+            String actualFileName = path.getFileName().toString();
+            logger.info("ExpectedFileName: {} ActualFileName: {} ", expectedFileName, actualFileName);
             Assertions.assertEquals(expectedFileName, actualFileName);
         }
     }
@@ -98,7 +99,6 @@ class AuditManagerTest {
     @ParameterizedTest(name = "Records add: {0}")
     @ValueSource(ints = {0, 1, 2, 3, 4, 5, 11})
     void checkContentFile(int records) throws IOException {
-
         for (int i = 0; i < records; i++) {
             auditManager.addRecord("user" + (i + 1), LocalDateTime.now());
         }
@@ -108,16 +108,15 @@ class AuditManagerTest {
         }
         int count = 1;
         for (Path filePath : filePaths) {
+            logger.info("FileName: {}", filePath.getFileName());
             var list1 = Files.readAllLines(filePath);
-            System.out.println(filePath.getFileName().toString());
             for (String record : list1) {
                 String expected = "user" + count++;
-                System.out.println(record);
+                logger.info("Line: {} Expected startsWith: {}", record, expected);
                 Assertions.assertTrue(record.startsWith(expected));
             }
         }
     }
-
 
     @AfterEach
     void TearDown() throws IOException {
